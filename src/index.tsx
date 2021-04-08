b05746547e27543e2aa587c705c0d4f91319ddac
zq19920306 <1113638674@qq.com>
2021-04-08 16:39:03 +0800

    init

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDom from "react-dom";
-
-import { Hello } from "./componets/hello";
-
-ReactDom.render(
-	<Hello compiler="Typescript" framework="React" />,
-	document.getElementById("root")
-);
